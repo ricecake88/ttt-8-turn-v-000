846bebf4192723185d2afec8f917c6908d496a11
Grace Shih <vocalise@gmail.com>
2018-04-26 17:50:14 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,7 +9,11 @@ end
 def valid_move?(board, index)
   if (board[index] == "X" || board[index] == "O" || index > 8 || index < 0 || position_taken?(board, index))
     return false
+<<<<<<< HEAD
   else
+=======
+  elsif (!position_taken?(board,index))
+>>>>>>> e1945ca748556b9dcd37c1747033af424a5aa73a
     return true
   end
 end
@@ -34,7 +38,12 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+<<<<<<< HEAD
   num = input_to_index(gets.strip)
+=======
+  index = gets.strip
+  num = input_to_index(index)
+>>>>>>> e1945ca748556b9dcd37c1747033af424a5aa73a
   if (num < 9 && num >= 0)
       move(board, num)
       display_board(board)
